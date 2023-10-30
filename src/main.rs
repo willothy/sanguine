3861fb4e512c5f3f433cf53f872c1f409aab59e3
Will Hopkins <willothyh@gmail.com>
2023-10-30 04:33:34 -0700

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,30 @@
-use std::{future::Future, task, time::Duration};
+use std::{
+    future::Future,
+    os::fd::{AsRawFd, FromRawFd, RawFd},
+    task,
+    time::Duration,
+};
 
-use anyhow::Result;
+use anyhow::{anyhow, Result};
+use dashmap::DashMap;
 use slotmap::{new_key_type, SlotMap};
 use std::task::Poll;
 use stretch::{
     geometry::Size,
-    node::Node,
+    node::{MeasureFunc, Node},
     number::Number,
-    style::{Dimension, JustifyContent, Style},
+    style::{Dimension, Direction, FlexDirection, JustifyContent, Style},
     Stretch,
 };
 use termwiz::{
     input::InputEvent,
-    surface::Surface,
+    surface::{Change, Position, Surface},
     terminal::{buffered::BufferedTerminal, new_terminal, Terminal},
+};
+use tokio::{
+    io::{stdin, AsyncReadExt},
+    net::unix::OwnedReadHalf,
+    time::interval,
 };
 
 pub enum LayoutAxis {
@@ -23,7 +34,6 @@ pub enum LayoutAxis {
 
 pub enum Frame {
     Leaf {
-        win: WindowId,
         node: Node,
     },
     Container {
@@ -33,46 +43,76 @@ pub enum Frame {
     },
 }
 
+impl Frame {
+    pub fn node(&self) -> Node {
+        match *self {
+            Frame::Leaf { node, .. } => node,
+            Frame::Container { node, .. } => node,
+        }
+    }
+}
+
 new_key_type! {
     pub struct WindowId;
 }
 
+trait ToAnyhow<T, E> {
+    fn to_anyhow(self) -> Result<T, anyhow::Error>;
+}
+
+impl<T> ToAnyhow<T, stretch::Error> for Result<T, stretch::Error> {
+    fn to_anyhow(self) -> Result<T, anyhow::Error> {
+        self.map_err(|e| anyhow!("{e}"))
+    }
+}
+
 pub struct Window {
-    pub handle: WindowId,
+    pub handle: Node,
     surface: Surface,
 }
 
 pub struct WindowManager {
     pub root: Frame,
     pub layout: Stretch,
-    pub windows: SlotMap<WindowId, Window>,
+    pub windows: DashMap<Node, Window>,
+    win_parents: DashMap<Node, Node>,
     pub floating: Vec<WindowId>,
 }
 
 impl WindowManager {
     pub fn new() -> Self {
         let mut layout = Stretch::new();
-        let mut windows = SlotMap::<WindowId, _>::with_key();
-        let first_win = windows.insert_with_key(|k| Window {
-            handle: k,
-            surface: Surface::new(1, 1),
-        });
+        let mut windows = DashMap::new();
         let node = layout
-            .new_node(
+            .new_leaf(
                 Style {
                     size: Size {
-                        width: Dimension::Auto,
-                        height: Dimension::Auto,
+                        width: Dimension::Percent(1.0),
+                        height: Dimension::Percent(1.0),
                     },
                     ..Default::default()
                 },
-                vec![],
+                Box::new(|s| {
+                    let width = match s.width {
+                        Number::Defined(w) => w,
+                        Number::Undefined => 0.0,
+                    };
+                    let height = match s.height {
+                        Number::Defined(h) => h,
+                        Number::Undefined => 0.0,
+                    };
+                    Ok(Size { width, height })
+                }),
             )
             .unwrap();
-        let root = Frame::Leaf {
-            win: first_win,
+        let first_win = windows.insert(
             node,
-        };
+            Window {
+                surface: Surface::new(1, 1),
+                handle: node,
+            },
+        );
+        let root = Frame::Leaf { node };
         Self {
             root,
             windows,
@@ -80,9 +120,136 @@ impl WindowManager {
             floating: Vec::new(),
         }
     }
+
+    pub fn recompute_layout(&mut self) -> Result<()> {
+        if self.layout.dirty(self.root.node()).to_anyhow()? {
+            self.layout
+                .compute_layout(self.root.node(), Size::undefined())
+                .to_anyhow()?;
+        }
+        todo!()
+    }
+
+    pub fn split(&mut self, handle: Node) -> Result<()> {
+        self.recompute_layout()?;
+        let window = self
+            .windows
+            .get(&handle)
+            .ok_or_else(|| anyhow!("Invalid window {handle:?}"))?;
+        // let layout = self.layout.layout(handle).to_anyhow()?;
+        // let parent = handle.
+
+        // let new_node =self.layout.new_lea
+
+        Ok(())
+    }
+
+    pub fn render(&mut self, surface: &mut Surface) -> Result<()> {
+        let (width, height) = surface.dimensions();
+        let size = Size {
+            width: Number::Defined(width as f32),
+            height: Number::Defined(height as f32),
+        };
+
+        self.layout
+            .compute_layout(self.root.node(), size)
+            .map_err(|e| anyhow!("{e}"))?;
+
+        let mut stack = vec![&self.root];
+        loop {
+            let node = match stack.pop() {
+                Some(Frame::Leaf { node }) => *node,
+                Some(Frame::Container { children, .. }) => {
+                    stack.extend(children.iter().rev());
+                    continue;
+                }
+                None => break,
+            };
+            let dims = self.layout.layout(node).map_err(|e| anyhow!("{e}"))?;
+            let Some(mut win) = self.windows.get_mut(&node) else {
+                continue;
+            };
+            let win_dims = (
+                dims.size.width.round() as usize,
+                dims.size.height.round() as usize,
+            );
+            win.surface.resize(win_dims.0, win_dims.1);
+            win.surface.draw_border();
+            surface.draw_from_screen(
+                &win.surface,
+                dims.location.x.round() as usize,
+                dims.location.y.round() as usize,
+            );
+        }
+
+        Ok(())
+    }
 }
 
-struct PollInput<'a, T>(&'a mut T)
+pub trait DrawBorder {
+    const HORIZONTAL: char = '─';
+    const VERTICAL: char = '│';
+    const TOP_LEFT: char = '┌';
+    const TOP_RIGHT: char = '┐';
+    const BOTTOM_LEFT: char = '└';
+    const BOTTOM_RIGHT: char = '┘';
+
+    fn draw_border(&mut self);
+}
+
+impl DrawBorder for Surface {
+    fn draw_border(&mut self) {
+        let (w, h) = self.dimensions();
+
+        let horz = std::iter::repeat(Self::HORIZONTAL)
+            .take(w.saturating_sub(2))
+            .collect::<String>();
+
+        let (x, y) = self.cursor_position();
+
+        self.add_changes(vec![
+            Change::CursorPosition {
+                x: Position::Absolute(0),
+                y: Position::Absolute(0),
+            },
+            Change::Text(format!("{}{}{}", Self::TOP_LEFT, horz, Self::TOP_RIGHT)),
+        ]);
+
+        for y in 1..h - 1 {
+            self.add_changes(vec![
+                Change::CursorPosition {
+                    x: Position::Absolute(0),
+                    y: Position::Absolute(y),
+                },
+                Change::Text(format!("{}", Self::VERTICAL)),
+                Change::CursorPosition {
+                    x: Position::Absolute(w.saturating_sub(1)),
+                    y: Position::Absolute(y),
+                },
+                Change::Text(format!("{}", Self::VERTICAL)),
+            ]);
+        }
+
+        self.add_changes(vec![
+            Change::CursorPosition {
+                x: Position::Absolute(0),
+                y: Position::Absolute(h.saturating_sub(1)),
+            },
+            Change::Text(format!(
+                "{}{}{}",
+                Self::BOTTOM_LEFT,
+                horz,
+                Self::BOTTOM_RIGHT
+            )),
+            Change::CursorPosition {
+                x: Position::Absolute(x),
+                y: Position::Absolute(y),
+            },
+        ]);
+    }
+}
+
+struct PollInput<'a, T>(&'a mut T, tokio::time::Interval)
 where
     T: termwiz::terminal::Terminal + Send;
 
@@ -96,7 +263,9 @@ where
         match self.0.poll_input(Some(Duration::ZERO)) {
             Ok(Some(v)) => Poll::Ready(Ok(v)),
             Ok(None) => {
-                cx.waker().wake_by_ref();
+                if self.1.poll_tick(cx).is_ready() {
+                    cx.waker().wake_by_ref();
+                }
                 Poll::Pending
             }
             Err(e) => Poll::Ready(Err(e.into())),
@@ -118,7 +287,7 @@ where
     T: termwiz::terminal::Terminal + Send,
 {
     async fn poll_input_async(&mut self) -> Result<InputEvent> {
-        PollInput(self).await
+        PollInput(self, interval(Duration::from_millis(1))).await
     }
 }
 
@@ -128,7 +297,11 @@ async fn main() -> Result<()> {
     let mut term = new_terminal(caps)?;
     term.set_raw_mode()?;
     let mut buffered = BufferedTerminal::new(term)?;
-    let input = buffered.terminal().poll_input_async().await?;
+    let mut wm = WindowManager::new();
+    wm.render(&mut buffered)?;
+    buffered.flush()?;
+    let input = stdin().read_u8().await?;
+    // let input = buffered.terminal().poll_input_async().await?;
     println!("Got input {:?}", input);
     Ok(())
 }
